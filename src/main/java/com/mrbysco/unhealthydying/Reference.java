2873379248ca2be9f8069c997516b61406c70499
Mrbysco <Mrbysco@users.noreply.github.com>
2021-01-01 22:05:11 +0100

    -

--- src/main/java/com/mrbysco/unhealthydying/Reference.java
+++ src/main/java/com/mrbysco/unhealthydying/Reference.java
@@ -5,6 +5,6 @@ import java.util.UUID;
 public class Reference {
 	public static final String MOD_ID = "unhealthydying";
 	public static final String MOD_PREFIX = MOD_ID + ":";
-	
+
 	public static final UUID HEALTH_MODIFIER_ID = UUID.fromString("F0FFC9E3-1EF7-4DC7-A1CE-20B55192AA97");
 }
