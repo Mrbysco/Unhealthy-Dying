b0cc230a9c43973c8b64d1bbc3cbcbde7afd48f0
Mrbysco <Mrbysco@users.noreply.github.com>
2023-03-18 19:27:05 +0100

    Fix health message

--- src/main/java/com/mrbysco/unhealthydying/util/HealthUtil.java
+++ src/main/java/com/mrbysco/unhealthydying/util/HealthUtil.java
@@ -9,11 +9,11 @@ import net.minecraft.world.entity.player.Player;
 public class HealthUtil {
 	public static void sendHealthMessage(Player player, int newHealth, int gained) {
 		if (gained >= 0 && UnhealthyConfig.SERVER.regenHealthMessage.get()) {
-			MutableComponent text = Component.translatable("unhealthydying:regennedHealth.message", newHealth + gained).withStyle(ChatFormatting.DARK_GREEN);
+			MutableComponent text = Component.translatable("unhealthydying:regennedHealth.message", newHealth).withStyle(ChatFormatting.DARK_GREEN);
 			player.displayClientMessage(text, true);
 		} else {
 			if (UnhealthyConfig.SERVER.reducedHealthMessage.get()) {
-				MutableComponent text = Component.translatable("unhealthydying:reducedHealth.message", newHealth + gained).withStyle(ChatFormatting.DARK_RED);
+				MutableComponent text = Component.translatable("unhealthydying:reducedHealth.message", newHealth).withStyle(ChatFormatting.DARK_RED);
 				player.displayClientMessage(text, true);
 			}
 		}
