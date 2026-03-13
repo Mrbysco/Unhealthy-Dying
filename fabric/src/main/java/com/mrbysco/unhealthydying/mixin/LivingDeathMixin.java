4dc1fc17e5bddb0f10fdd5eaa5d0c57225a0d84c
Mrbysco <Mrbysco@users.noreply.github.com>
2026-03-13 23:32:49 +0100

    Fix mixin

--- fabric/src/main/java/com/mrbysco/unhealthydying/mixin/LivingDeathMixin.java
+++ fabric/src/main/java/com/mrbysco/unhealthydying/mixin/LivingDeathMixin.java
@@ -1,16 +1,14 @@
 package com.mrbysco.unhealthydying.mixin;
 
 import com.mrbysco.unhealthydying.handlers.EasterEgg;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = {Player.class, ServerPlayer.class})
+@Mixin(LivingEntity.class)
 public class LivingDeathMixin {
 
 	@Inject(method = "die", at = @At("HEAD"))
